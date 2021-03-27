f108ffda22149140bb0406a3b18929f48e5dcca9
elijahb <maurolewinzon@gmail.com>
2021-03-26 21:00:54 -0300

    Implemented new paymail ext for payment destination with tokens support

---
+++ src/middlewares.js
@@ -0,0 +1,13 @@
+import { PaymailError } from './errors/PaymailError'
+import HttpStatus from 'http-status-codes'
+
+const checkContentType = (req, _res, next) => {
+  if (!req.is('application/json')) {
+    throw new PaymailError('Wrong content type. It should be `application/json`', HttpStatus.BAD_REQUEST, 'wrong-content-type')
+  }
+  next()
+}
+
+module.exports = {
+  checkContentType
+}
